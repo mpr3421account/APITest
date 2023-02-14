a66f69d290e49d7b16dc6e24097b0267f72655ae
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-14 08:36:23 -0300

    Add project files.

---
+++ APITest/Model/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITest.Model
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Adress { get; set; }
+
+
+    }
+}
